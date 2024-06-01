594e7d760121cfb5eeceaebf098dce50709d114f
sampsonnyebuk <inyangnyebuk@gmail.com>
2024-06-01 23:44:14 +0100

    Convert contactmanager from submodule to regular directory

---
+++ contactmanager/client/build/precache-manifest.4e1c0d3962d5f1312c0347b84768b01e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78698b734bd4c262689c345543e952e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b27827a4fee4608b074",
+    "url": "/static/css/2.b24350a8.chunk.css"
+  },
+  {
+    "revision": "0480b2408eb0cddfe99f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b27827a4fee4608b074",
+    "url": "/static/js/2.65cc99fa.chunk.js"
+  },
+  {
+    "revision": "6a7e784a68865c629af133999af2a34a",
+    "url": "/static/js/2.65cc99fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0480b2408eb0cddfe99f",
+    "url": "/static/js/main.f447af8a.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
---
+++ contactmanager/client/src/context/ToastContext.js
@@ -0,0 +1,35 @@
+// import { createContext } from "react";
+// import React from 'react';
+
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// const ToastContext = createContext();
+
+// export const ToastContextProvider = ({ children }) => {
+//   return (
+//     <ToastContext.Provider value={{ toast }}>
+//       <ToastContainer autoClose={2000} />
+//       {children}
+//     </ToastContext.Provider>
+//   );
+// };
+
+// export default ToastContext;
+import React from 'react';
+import { createContext } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastContext = createContext();
+
+export const ToastContextProvider = ({ children }) => {
+  return (
+    <ToastContext.Provider value={{ toast }}>
+      <ToastContainer autoClose={2000} />
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export default ToastContext;
